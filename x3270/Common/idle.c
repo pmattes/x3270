61d9232ba2cee4685fb1c96621bb7ded32fd05d2
Paul Mattes <paul.mattes@usa.net>
2008-02-29 02:58:22 +0000

    Pull up 3.3.7p4 fixes.
    git-svn-id: svn+ssh://svn.code.sf.net/p/x3270/x3270/trunk@102 12e4f418-770b-42d6-82dd-bd6eefd7344d

--- x3270/Common/idle.c
+++ x3270/Common/idle.c
@@ -130,9 +130,9 @@ idle_init(void)
 	register_schange(ST_CONNECT, idle_in3270);
 
 	/* Get values from resources. */
-	cmd = get_resource(ResIdleCommand);
+	cmd = appres.idle_command;
 	idle_command = cmd? NewString(cmd): CN;
-	tmo = get_resource(ResIdleTimeout);
+	tmo = appres.idle_timeout;
 	idle_timeout_string = tmo? NewString(tmo): CN;
 	if (appres.idle_command_enabled)
 		idle_user_enabled = IDLE_PERM;
