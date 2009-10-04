e80d910375d4194d3406d28345923aff997b0890
Paul Mattes <paul.mattes@usa.net>
2009-10-04 04:14:28 +0000

    c3270 menus, another snapshot.
    git-svn-id: svn+ssh://svn.code.sf.net/p/x3270/x3270/trunk/x3270@612 12e4f418-770b-42d6-82dd-bd6eefd7344d

--- c3270/ckeypadc.h
+++
@@ -1,6 +0,0 @@
-/* Global declarations for keypad.c (which should be ckeypad.c) */
-extern Boolean keypad_is_up;
-extern Boolean keypad_char(int row, int col, ucs4_t *u, Boolean *highlighted);
-extern void keypad_cursor(int *row, int *col);
-extern void pop_up_keypad(Boolean up);
-extern void keypad_key(int k, ucs4_t u);
