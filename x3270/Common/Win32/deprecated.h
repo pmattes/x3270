a7df6c2ce858192f30133f2a873a6400a51654b3
Paul Mattes <paul.mattes@usa.net>
2013-04-15 23:28:25 +0000

    fewer VC warnings
    git-svn-id: svn+ssh://svn.code.sf.net/p/x3270/x3270/trunk@850 12e4f418-770b-42d6-82dd-bd6eefd7344d

--- x3270/Common/Win32/deprecated.h
+++ x3270/Common/Win32/deprecated.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007-2009, Paul Mattes.
+ * Copyright (c) 2007-2013, Paul Mattes.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -28,12 +28,19 @@
 /*
  * Redefinitions of POSIX functions that MSC doesn't like the names of.
  */
+#include <io.h>		/* for _close, _dup, _read, _write */
+#include <process.h>	/* for _getpid */
+#include <stdio.h>	/* for _fdopen, _fileno, _snprintf */
 
-#define close		_close
-#define fdopen		_fdopen
-#define fileno		_fileno
-#define getcwd		_getcwd
-#define open		_open
-#define putenv		_putenv
-#define snprintf	_snprintf
-#define unlink		_unlink
+#define close           _close
+#define dup             _dup
+#define fdopen          _fdopen
+#define fileno          _fileno
+#define getpid          _getpid
+#define getcwd          _getcwd
+#define open            _open
+#define putenv          _putenv
+#define read            _read
+#define snprintf        _snprintf
+#define unlink          _unlink
+#define write           _write
