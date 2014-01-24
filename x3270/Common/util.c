e54f6acf272426a0d05731aff1c84a25853de246
Paul Mattes <paul.mattes@usa.net>
2014-01-24 05:26:49 +0000

    Pull up 3.3.14 fix
    git-svn-id: svn+ssh://svn.code.sf.net/p/x3270/x3270/trunk@1211 12e4f418-770b-42d6-82dd-bd6eefd7344d

--- x3270/Common/util.c
+++ x3270/Common/util.c
@@ -1183,7 +1183,7 @@ dump_version(void)
 	printf("%s\n%s\n", build, build_options());
 	charset_list();
 	printf("\n"
-"Copyright 1989-2013, Paul Mattes, GTRC and others.\n"
+"Copyright 1989-2014, Paul Mattes, GTRC and others.\n"
 "See the source code or documentation for licensing details.\n"
 "Distributed WITHOUT ANY WARRANTY; without even the implied warranty of\n"
 "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n");
