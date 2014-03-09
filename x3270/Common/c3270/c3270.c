2765973bc0120111e6e5a20c58c32600b2138b8f
Paul Mattes <paul.mattes@usa.net>
2014-03-09 14:43:50 +0000

    Fix readline 6.3 issue
    git-svn-id: svn+ssh://svn.code.sf.net/p/x3270/x3270/branches/3.3.14@1246 12e4f418-770b-42d6-82dd-bd6eefd7344d

--- x3270/Common/c3270/c3270.c
+++ x3270/Common/c3270/c3270.c
@@ -100,7 +100,7 @@ static void interact(void);
 static void stop_pager(void);
 
 #if defined(HAVE_LIBREADLINE) /*[*/
-static CPPFunction attempted_completion;
+static char **attempted_completion();
 static char *completion_entry(const char *, int);
 #endif /*]*/
 
