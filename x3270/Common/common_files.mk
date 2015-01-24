0854317da80edb3265b1b22aacc5c4a928ae8e0d
Paul Mattes <paul.mattes@usa.net>
2015-01-24 05:33:13 +0000

    Moved resolver.c to lib/
    git-svn-id: svn+ssh://svn.code.sf.net/p/x3270/x3270/trunk@1598 12e4f418-770b-42d6-82dd-bd6eefd7344d

--- x3270/Common/common_files.mk
+++ x3270/Common/common_files.mk
@@ -1,8 +1,8 @@
 # Source files common to all 3270 emulators
 COMMON_SOURCES = apl.c charset.c ctlr.c fprint_screen.c ft.c ft_cut.c \
 	ft_dft.c host.c idle.c kybd.c linemode.c nvt.c print_screen.c proxy.c \
-	resolver.c resources.c rpq.c sf.c tables.c telnet.c toggles.c trace.c \
-	unicode.c unicode_dbcs.c util.c xio.c
+	resources.c rpq.c sf.c tables.c telnet.c toggles.c trace.c unicode.c \
+	unicode_dbcs.c util.c xio.c
 
 # HTTPD source files
 HTTPD_SOURCES = bind-opt.c favicon.c httpd-core.c httpd-io.c httpd-nodes.c
@@ -11,9 +11,9 @@ HTTPD_SOURCES = bind-opt.c favicon.c httpd-core.c httpd-io.c httpd-nodes.c
 COMMON_OBJECTS = apl.$(OBJ) charset.$(OBJ) ctlr.$(OBJ) fprint_screen.$(OBJ) \
 	ft.$(OBJ) ft_cut.$(OBJ) ft_dft.$(OBJ) host.$(OBJ) idle.$(OBJ) \
 	kybd.$(OBJ) linemode.$(OBJ) nvt.$(OBJ) print_screen.$(OBJ) \
-	proxy.$(OBJ) resolver.$(OBJ) resources.$(OBJ) rpq.$(OBJ) sf.$(OBJ) \
-	tables.$(OBJ) telnet.$(OBJ) toggles.$(OBJ) trace.$(OBJ) \
-	unicode.$(OBJ) unicode_dbcs.$(OBJ) util.$(OBJ) xio.$(OBJ)
+	proxy.$(OBJ) resources.$(OBJ) rpq.$(OBJ) sf.$(OBJ) tables.$(OBJ) \
+	telnet.$(OBJ) toggles.$(OBJ) trace.$(OBJ) unicode.$(OBJ) \
+	unicode_dbcs.$(OBJ) util.$(OBJ) xio.$(OBJ)
 
 # HTTPD object files
 HTTPD_OBJECTS = bind-opt.$(OBJ) favicon.$(OBJ) httpd-core.$(OBJ) \
--- x3270/Common/lib3270_files.mk
+++ x3270/Common/lib3270_files.mk
@@ -1,9 +1,10 @@
 # Source files for lib3270.
-LIB3270_SOURCES = Malloc.c actions.c asprintf.c lazya.c see.c utf8.c varbuf.c
+LIB3270_SOURCES = Malloc.c actions.c asprintf.c lazya.c resolver.c see.c \
+	utf8.c varbuf.c
 
 # Object files for lib3270.
 LIB3270_OBJECTS = Malloc.$(OBJ) actions.$(OBJ) asprintf.$(OBJ) lazya.$(OBJ) \
-	see.$(OBJ) utf8.$(OBJ) varbuf.$(OBJ)
+	resolver.$(OBJ) see.$(OBJ) utf8.$(OBJ) varbuf.$(OBJ)
 
 # Header files for lib3270.
 LIB3270_HEADERS = conf.h localdefs.h
