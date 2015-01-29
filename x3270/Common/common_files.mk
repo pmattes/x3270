ffc208bc867a6c34072d2efb2e7c17ae2f61822a
Paul Mattes <paul.mattes@usa.net>
2015-01-29 05:52:25 +0000

    Move linemode.c to the library
    git-svn-id: svn+ssh://svn.code.sf.net/p/x3270/x3270/trunk@1652 12e4f418-770b-42d6-82dd-bd6eefd7344d

--- x3270/Common/common_files.mk
+++ x3270/Common/common_files.mk
@@ -1,18 +1,17 @@
 # Source files common to all 3270 emulators
-COMMON_SOURCES = charset.c ctlr.c ft.c ft_dft.c host.c idle.c kybd.c \
-	linemode.c nvt.c print_screen.c proxy.c resources.c rpq.c sf.c \
-	tables.c telnet.c toggles.c trace.c unicode.c unicode_dbcs.c util.c \
-	xio.c
+COMMON_SOURCES = charset.c ctlr.c ft.c ft_dft.c host.c idle.c kybd.c nvt.c \
+	print_screen.c proxy.c resources.c rpq.c sf.c tables.c telnet.c \
+	toggles.c trace.c unicode.c unicode_dbcs.c util.c xio.c
 
 # HTTPD source files
 HTTPD_SOURCES = favicon.c httpd-core.c httpd-io.c httpd-nodes.c
 
 # Object files common to all 3270 emulators
 COMMON_OBJECTS = charset.$(OBJ) ctlr.$(OBJ) ft.$(OBJ) ft_dft.$(OBJ) \
-	host.$(OBJ) idle.$(OBJ) kybd.$(OBJ) linemode.$(OBJ) nvt.$(OBJ) \
-	print_screen.$(OBJ) proxy.$(OBJ) resources.$(OBJ) rpq.$(OBJ) \
-	sf.$(OBJ) tables.$(OBJ) telnet.$(OBJ) toggles.$(OBJ) trace.$(OBJ) \
-	unicode.$(OBJ) unicode_dbcs.$(OBJ) util.$(OBJ) xio.$(OBJ)
+	host.$(OBJ) idle.$(OBJ) kybd.$(OBJ) nvt.$(OBJ) print_screen.$(OBJ) \
+	proxy.$(OBJ) resources.$(OBJ) rpq.$(OBJ) sf.$(OBJ) tables.$(OBJ) \
+	telnet.$(OBJ) toggles.$(OBJ) trace.$(OBJ) unicode.$(OBJ) \
+	unicode_dbcs.$(OBJ) util.$(OBJ) xio.$(OBJ)
 
 # HTTPD object files
 HTTPD_OBJECTS = favicon.$(OBJ) httpd-core.$(OBJ) httpd-io.$(OBJ) \
--- x3270/Common/lib3270_files.mk
+++ x3270/Common/lib3270_files.mk
@@ -1,13 +1,13 @@
 # Source files for lib3270.
 LIB3270_SOURCES = Malloc.c actions.c apl.c asprintf.c bind-opt.c \
 	fprint_screen.c ft_cut.c httpd-core.c httpd-io.c httpd-nodes.c \
-	lazya.c resolver.c see.c utf8.c varbuf.c
+	lazya.c linemode.c resolver.c see.c utf8.c varbuf.c
 
 # Object files for lib3270.
 LIB3270_OBJECTS = Malloc.$(OBJ) actions.$(OBJ) apl.$(OBJ) asprintf.$(OBJ) \
 	bind-opt.$(OBJ) fprint_screen.$(OBJ) ft_cut.$(OBJ) httpd-core.$(OBJ) \
-	httpd-io.$(OBJ) httpd-nodes.$(OBJ) lazya.$(OBJ) resolver.$(OBJ) \
-	see.$(OBJ) utf8.$(OBJ) varbuf.$(OBJ)
+	httpd-io.$(OBJ) httpd-nodes.$(OBJ) lazya.$(OBJ) linemode.$(OBJ) \
+	resolver.$(OBJ) see.$(OBJ) utf8.$(OBJ) varbuf.$(OBJ)
 
 # Header files for lib3270.
 LIB3270_HEADERS = conf.h localdefs.h
