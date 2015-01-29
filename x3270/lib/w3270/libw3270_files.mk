6845a38fddf6e49fdf24dd8a99a65ce78ba76a90
Paul Mattes <paul.mattes@usa.net>
2015-01-29 05:48:19 +0000

    Move gdi_print.c to the Windows library
    git-svn-id: svn+ssh://svn.code.sf.net/p/x3270/x3270/trunk@1651 12e4f418-770b-42d6-82dd-bd6eefd7344d

--- x3270/lib/w3270/libw3270_files.mk
+++ x3270/lib/w3270/libw3270_files.mk
@@ -1,9 +1,9 @@
 # Source files specific to libw3270.
-LIBW3270_SOURCES = snprintf.c ssl_dll.c w3misc.c winprint.c
+LIBW3270_SOURCES = gdi_print.c snprintf.c ssl_dll.c w3misc.c winprint.c
 
 # Object files specific to libw3270.
-LIBW3270_OBJECTS = snprintf.$(OBJ) ssl_dll.$(OBJ) w3misc.$(OBJ) \
-	winprint.$(OBJ)
+LIBW3270_OBJECTS = gdi_print.$(OBJ) snprintf.$(OBJ) ssl_dll.$(OBJ) \
+	w3misc.$(OBJ) winprint.$(OBJ)
 
 # Header files specific to libw3270.
 LIBW3270_HEADERS = 
--- x3270/ws3270/ws3270_files.mk
+++ x3270/ws3270/ws3270_files.mk
@@ -1,10 +1,9 @@
 # ws3270-specific source files
-WS3270_SOURCES = XtGlue.c gdi_print.c glue.c macros.c readres.c s3270.c \
-	strtok_r.c windirs.c winvers.c
+WS3270_SOURCES = XtGlue.c glue.c macros.c readres.c s3270.c strtok_r.c \
+	windirs.c winvers.c
 
-WS3270_OBJECTS = XtGlue.$(OBJ) gdi_print.$(OBJ) glue.$(OBJ) macros.$(OBJ) \
-	readres.$(OBJ) s3270.$(OBJ) strtok_r.$(OBJ) windirs.$(OBJ) \
-	winvers.$(OBJ)
+WS3270_OBJECTS = XtGlue.$(OBJ) glue.$(OBJ) macros.$(OBJ) readres.$(OBJ) \
+	s3270.$(OBJ) strtok_r.$(OBJ) windirs.$(OBJ) winvers.$(OBJ)
 
 # ws3270-specific header files
 WS3270_HEADERS = conf.h gluec.h readresc.h windirsc.h winversc.h
