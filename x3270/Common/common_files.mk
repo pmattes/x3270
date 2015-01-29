3ea798fa05c5d8dd426243ef802636dcc8fcd73a
Paul Mattes <paul.mattes@usa.net>
2015-01-29 06:12:27 +0000

    Move ft/dt_dft.c to the library; fix pr3287 tables issue
    git-svn-id: svn+ssh://svn.code.sf.net/p/x3270/x3270/trunk@1655 12e4f418-770b-42d6-82dd-bd6eefd7344d

--- x3270/Common/common_files.mk
+++ x3270/Common/common_files.mk
@@ -1,17 +1,16 @@
 # Source files common to all 3270 emulators
-COMMON_SOURCES = charset.c ctlr.c ft.c ft_dft.c host.c idle.c kybd.c nvt.c \
-	print_screen.c proxy.c resources.c rpq.c sf.c telnet.c trace.c \
-	unicode.c unicode_dbcs.c util.c xio.c
+COMMON_SOURCES = charset.c ctlr.c host.c idle.c kybd.c nvt.c print_screen.c \
+	proxy.c resources.c rpq.c sf.c telnet.c trace.c unicode.c \
+	unicode_dbcs.c util.c xio.c
 
 # HTTPD source files
 HTTPD_SOURCES = favicon.c httpd-core.c httpd-io.c httpd-nodes.c
 
 # Object files common to all 3270 emulators
-COMMON_OBJECTS = charset.$(OBJ) ctlr.$(OBJ) ft.$(OBJ) ft_dft.$(OBJ) \
-	host.$(OBJ) idle.$(OBJ) kybd.$(OBJ) nvt.$(OBJ) print_screen.$(OBJ) \
-	proxy.$(OBJ) resources.$(OBJ) rpq.$(OBJ) sf.$(OBJ) telnet.$(OBJ) \
-	trace.$(OBJ) unicode.$(OBJ) unicode_dbcs.$(OBJ) util.$(OBJ) \
-	xio.$(OBJ)
+COMMON_OBJECTS = charset.$(OBJ) ctlr.$(OBJ) host.$(OBJ) idle.$(OBJ) \
+	kybd.$(OBJ) nvt.$(OBJ) print_screen.$(OBJ) proxy.$(OBJ) \
+	resources.$(OBJ) rpq.$(OBJ) sf.$(OBJ) telnet.$(OBJ) trace.$(OBJ) \
+	unicode.$(OBJ) unicode_dbcs.$(OBJ) util.$(OBJ) xio.$(OBJ)
 
 # HTTPD object files
 HTTPD_OBJECTS = favicon.$(OBJ) httpd-core.$(OBJ) httpd-io.$(OBJ) \
