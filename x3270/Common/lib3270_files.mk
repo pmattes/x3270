649b67e749c16b19fed893a0cf051033e1eee938
Paul Mattes <paul.mattes@usa.net>
2015-02-03 23:44:14 +0000

    Move trace.c to the library
    git-svn-id: svn+ssh://svn.code.sf.net/p/x3270/x3270/trunk@1680 12e4f418-770b-42d6-82dd-bd6eefd7344d

--- x3270/Common/lib3270_files.mk
+++ x3270/Common/lib3270_files.mk
@@ -3,7 +3,7 @@ LIB3270_SOURCES = Malloc.c actions.c apl.c asprintf.c bind-opt.c charset.c \
 	ctlr.c fprint_screen.c ft.c ft_cut.c ft_dft.c httpd-core.c httpd-io.c \
 	httpd-nodes.c idle.c kybd.c lazya.c linemode.c print_screen.c proxy.c \
 	readres.c resolver.c rpq.c see.c sf.c tables.c telnet.c toggles.c \
-	unicode.c unicode_dbcs.c utf8.c varbuf.c
+	trace.c unicode.c unicode_dbcs.c utf8.c varbuf.c
 
 # Object files for lib3270.
 LIB3270_OBJECTS = Malloc.$(OBJ) actions.$(OBJ) apl.$(OBJ) asprintf.$(OBJ) \
@@ -12,8 +12,8 @@ LIB3270_OBJECTS = Malloc.$(OBJ) actions.$(OBJ) apl.$(OBJ) asprintf.$(OBJ) \
 	httpd-core.$(OBJ) httpd-io.$(OBJ) httpd-nodes.$(OBJ) idle.$(OBJ) \
 	kybd.$(OBJ) lazya.$(OBJ) linemode.$(OBJ) print_screen.$(OBJ) \
 	proxy.$(OBJ) readres.$(OBJ) resolver.$(OBJ) rpq.$(OBJ) see.$(OBJ) \
-	sf.$(OBJ) tables.$(OBJ) telnet.$(OBJ) toggles.$(OBJ) unicode.$(OBJ) \
-	unicode_dbcs.$(OBJ) utf8.$(OBJ) varbuf.$(OBJ)
+	sf.$(OBJ) tables.$(OBJ) telnet.$(OBJ) toggles.$(OBJ) trace.$(OBJ) \
+	unicode.$(OBJ) unicode_dbcs.$(OBJ) utf8.$(OBJ) varbuf.$(OBJ)
 
 # Header files for lib3270.
 LIB3270_HEADERS = conf.h localdefs.h
