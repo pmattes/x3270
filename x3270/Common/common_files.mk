7e1642f7c50e28d05db8e14191a0a77d48df8f6c
Paul Mattes <paul.mattes@usa.net>
2015-02-03 23:37:36 +0000

    Move telnet.c to the library
    git-svn-id: svn+ssh://svn.code.sf.net/p/x3270/x3270/trunk@1678 12e4f418-770b-42d6-82dd-bd6eefd7344d

--- x3270/Common/common_files.mk
+++ x3270/Common/common_files.mk
@@ -1,9 +1,9 @@
 # Source files common to all 3270 emulators
-COMMON_SOURCES = host.c nvt.c resources.c telnet.c trace.c util.c xio.c
+COMMON_SOURCES = host.c nvt.c resources.c trace.c util.c xio.c
 
 # Object files common to all 3270 emulators
-COMMON_OBJECTS = host.$(OBJ) nvt.$(OBJ) resources.$(OBJ) telnet.$(OBJ) \
-	trace.$(OBJ) util.$(OBJ) xio.$(OBJ)
+COMMON_OBJECTS = host.$(OBJ) nvt.$(OBJ) resources.$(OBJ) trace.$(OBJ) \
+	util.$(OBJ) xio.$(OBJ)
 
 # Header files common to all 3270 emulators
 COMMON_HEADERS = fallbacksc.h localdefs.h
--- x3270/Common/lib3270_files.mk
+++ x3270/Common/lib3270_files.mk
@@ -2,8 +2,8 @@
 LIB3270_SOURCES = Malloc.c actions.c apl.c asprintf.c bind-opt.c charset.c \
 	ctlr.c fprint_screen.c ft.c ft_cut.c ft_dft.c httpd-core.c httpd-io.c \
 	httpd-nodes.c idle.c kybd.c lazya.c linemode.c print_screen.c proxy.c \
-	readres.c resolver.c rpq.c see.c sf.c tables.c toggles.c unicode.c \
-	unicode_dbcs.c utf8.c varbuf.c
+	readres.c resolver.c rpq.c see.c sf.c tables.c telnet.c toggles.c \
+	unicode.c unicode_dbcs.c utf8.c varbuf.c
 
 # Object files for lib3270.
 LIB3270_OBJECTS = Malloc.$(OBJ) actions.$(OBJ) apl.$(OBJ) asprintf.$(OBJ) \
@@ -12,7 +12,7 @@ LIB3270_OBJECTS = Malloc.$(OBJ) actions.$(OBJ) apl.$(OBJ) asprintf.$(OBJ) \
 	httpd-core.$(OBJ) httpd-io.$(OBJ) httpd-nodes.$(OBJ) idle.$(OBJ) \
 	kybd.$(OBJ) lazya.$(OBJ) linemode.$(OBJ) print_screen.$(OBJ) \
 	proxy.$(OBJ) readres.$(OBJ) resolver.$(OBJ) rpq.$(OBJ) see.$(OBJ) \
-	sf.$(OBJ) tables.$(OBJ) toggles.$(OBJ) unicode.$(OBJ) \
+	sf.$(OBJ) tables.$(OBJ) telnet.$(OBJ) toggles.$(OBJ) unicode.$(OBJ) \
 	unicode_dbcs.$(OBJ) utf8.$(OBJ) varbuf.$(OBJ)
 
 # Header files for lib3270.
