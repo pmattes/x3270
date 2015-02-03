54e490dc46f12a876029779257cdc0e620d50ffa
Paul Mattes <paul.mattes@usa.net>
2015-02-03 17:29:06 +0000

    Move rpq.c to the library
    git-svn-id: svn+ssh://svn.code.sf.net/p/x3270/x3270/trunk@1662 12e4f418-770b-42d6-82dd-bd6eefd7344d

--- x3270/Common/common_files.mk
+++ x3270/Common/common_files.mk
@@ -1,12 +1,12 @@
 # Source files common to all 3270 emulators
 COMMON_SOURCES = charset.c ctlr.c host.c kybd.c nvt.c print_screen.c proxy.c \
-	resources.c rpq.c sf.c telnet.c trace.c unicode.c unicode_dbcs.c \
-	util.c xio.c
+	resources.c sf.c telnet.c trace.c unicode.c unicode_dbcs.c util.c \
+	xio.c
 
 # Object files common to all 3270 emulators
 COMMON_OBJECTS = charset.$(OBJ) ctlr.$(OBJ) host.$(OBJ) kybd.$(OBJ) \
 	nvt.$(OBJ) print_screen.$(OBJ) proxy.$(OBJ) resources.$(OBJ) \
-	rpq.$(OBJ) sf.$(OBJ) telnet.$(OBJ) trace.$(OBJ) unicode.$(OBJ) \
+	sf.$(OBJ) telnet.$(OBJ) trace.$(OBJ) unicode.$(OBJ) \
 	unicode_dbcs.$(OBJ) util.$(OBJ) xio.$(OBJ)
 
 # Header files common to all 3270 emulators
