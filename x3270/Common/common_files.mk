76596ba64e9299b5f5d231bf2a8679941c6bb9ee
Paul Mattes <paul.mattes@usa.net>
2015-02-03 17:46:25 +0000

    Moved ctlr.c to the library
    git-svn-id: svn+ssh://svn.code.sf.net/p/x3270/x3270/trunk@1665 12e4f418-770b-42d6-82dd-bd6eefd7344d

--- x3270/Common/common_files.mk
+++ x3270/Common/common_files.mk
@@ -1,12 +1,11 @@
 # Source files common to all 3270 emulators
-COMMON_SOURCES = charset.c ctlr.c host.c kybd.c nvt.c print_screen.c \
-	resources.c telnet.c trace.c unicode.c unicode_dbcs.c util.c xio.c
+COMMON_SOURCES = charset.c host.c kybd.c nvt.c print_screen.c resources.c \
+	telnet.c trace.c unicode.c unicode_dbcs.c util.c xio.c
 
 # Object files common to all 3270 emulators
-COMMON_OBJECTS = charset.$(OBJ) ctlr.$(OBJ) host.$(OBJ) kybd.$(OBJ) \
-	nvt.$(OBJ) print_screen.$(OBJ) resources.$(OBJ) telnet.$(OBJ) \
-	trace.$(OBJ) unicode.$(OBJ) unicode_dbcs.$(OBJ) util.$(OBJ) \
-	xio.$(OBJ)
+COMMON_OBJECTS = charset.$(OBJ) host.$(OBJ) kybd.$(OBJ) nvt.$(OBJ) \
+	print_screen.$(OBJ) resources.$(OBJ) telnet.$(OBJ) trace.$(OBJ) \
+	unicode.$(OBJ) unicode_dbcs.$(OBJ) util.$(OBJ) xio.$(OBJ)
 
 # Header files common to all 3270 emulators
 COMMON_HEADERS = fallbacksc.h localdefs.h
