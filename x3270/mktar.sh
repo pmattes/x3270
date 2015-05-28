08bc274c4775449a544007d68f3367a52acdc391
Paul Mattes <paul.mattes@usa.net>
2015-05-28 15:04:31 +0000

    Pull trunk fix into 3.4
    git-svn-id: svn+ssh://svn.code.sf.net/p/x3270/x3270/branches/3.4@1877 12e4f418-770b-42d6-82dd-bd6eefd7344d

--- x3270/mktar.sh
+++ x3270/mktar.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 . Common/version.txt
 ver=${version%%[a-z]*}
-tar -czf suite3270-$version-src.tgz -T manifest --transform "s@^@suite3270-$ver/@" -h
+tar -czf suite3270-$version-src.tgz -T manifest --transform "s@^@suite3270-$ver/@" -h --hard-dereference
