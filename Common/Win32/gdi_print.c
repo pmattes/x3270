9ee036a6ffc99c7dca50af605358415cdadc2dc3
Paul Mattes <paul.mattes@usa.net>
2017-12-04 15:12:36 -0600

    Copyright nits

--- Common/Win32/gdi_print.c
+++ Common/Win32/gdi_print.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1994-2015 Paul Mattes.
+ * Copyright (c) 1994-2017 Paul Mattes.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
