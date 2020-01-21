684e226cf669941a05d9cda706f838f9485286cc
Paul Mattes <paul.mattes@usa.net>
2020-01-20 22:50:55 -0600

    Add missing state change name

--- Common/event.c
+++ Common/event.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1993-2015, 2019 Paul Mattes.
+ * Copyright (c) 1993-2015, 2019-2020 Paul Mattes.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -117,7 +117,8 @@ st_changed(enum st tx, bool mode)
 	"codepage",
 	"selecting",
 	"secure",
-	"kbd-disable"
+	"kbd-disable",
+	"terminal-name"
     };
     struct st_callback *st;
 
