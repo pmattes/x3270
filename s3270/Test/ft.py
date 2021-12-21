1cbb4c9f8c89bcc62dedfcaacbef5e89f7d459d6
Paul Mattes <paul.mattes@case.edu>
2021-12-21 17:37:47 -0600

    Complete CUT-mode file transfer test

--- s3270/Test/ft.py
+++ s3270/Test/ft.py
@@ -8,7 +8,7 @@ import requests
 class TestS3270ft(unittest.TestCase):
 
     # s3270 DFT-mode file transfer test
-    def test_s3270_ft_cut(self):
+    def test_s3270_ft_dft(self):
 
         # Start 'playback' to read s3270's output.
         playback = Popen(["playback", "-b", "-p", "9991",
@@ -29,26 +29,27 @@ class TestS3270ft(unittest.TestCase):
         s3270.wait(timeout=2)
 
     # s3270 CUT-mode file transfer test
-#    def test_s3270_ft_cut(self):
-#
-#        # Start 'playback' to read s3270's output.
-#        playback = Popen(["playback", "-b", "-p", "9990",
-#            "s3270/Test/ft_cut.trc"], stdout=DEVNULL)
-#
-#        # Start s3270.
-#        s3270 = Popen(["s3270", "127.0.0.1:9990"], stdin=PIPE, stdout=DEVNULL)
-#
-#        # Feed s3270 some actions.
-#        s3270.stdin.write(b'transfer direction=send host=vm "localfile=s3270/Test/fttext" "hostfile=ibmlink trc a"\n')
-#        s3270.stdin.write(b"String(logoff)\n")
-#        s3270.stdin.write(b"Enter()\n")
-#        s3270.stdin.flush()
-#
-#        # Wait for the processes to exit.
-#        rc = playback.wait(timeout=2)
-#        self.assertEqual(rc, 0)
-#        s3270.stdin.close()
-#        s3270.wait(timeout=2)
+    def test_s3270_ft_cut(self):
+
+        # Start 'playback' to read s3270's output.
+        playback = Popen(["playback", "-b", "-p", "9990",
+            "s3270/Test/ft_cut.trc"], stdout=DEVNULL)
+
+        # Start s3270.
+        s3270 = Popen(["s3270", "-model", "2", "127.0.0.1:9990"], stdin=PIPE,
+                stdout=DEVNULL)
+
+        # Feed s3270 some actions.
+        s3270.stdin.write(b'transfer direction=send host=vm "localfile=s3270/Test/fttext" "hostfile=ft text a"\n')
+        s3270.stdin.write(b"String(logoff)\n")
+        s3270.stdin.write(b"Enter()\n")
+        s3270.stdin.flush()
+
+        # Wait for the processes to exit.
+        rc = playback.wait(timeout=2)
+        self.assertEqual(rc, 0)
+        s3270.stdin.close()
+        s3270.wait(timeout=2)
 
 if __name__ == '__main__':
     unittest.main()
