39ad438b2e8efa99f168ec582086a5f08cc52480
Paul Mattes <paul.mattes@case.edu>
2021-12-29 18:47:43 -0600

    Fix wc3270 packaging scripts

--- wc3270/CopyToWx3270.ps1
+++ wc3270/CopyToWx3270.ps1
@@ -9,8 +9,9 @@ if (-Not $x) {
    exit 1
 }
 foreach ($i in @('win32', 'win64')) {
-   copy obj\$i\wb3270\b3270.exe $Destdir\extern\x3270-$i
-   copy obj\$i\ws3270\s3270.exe $Destdir\extern\x3270-$i
-   copy obj\$i\wpr3287\pr3287.exe $Destdir\extern\x3270-$i
+   copy obj\$i\playback\playback.exe $Destdir\extern\x3270-$i
+   copy obj\$i\b3270\b3270.exe $Destdir\extern\x3270-$i
+   copy obj\$i\pr3287\pr3287.exe $Destdir\extern\x3270-$i
+   copy obj\$i\s3270\s3270.exe $Destdir\extern\x3270-$i
    copy obj\$i\x3270if\x3270if.exe $Destdir\extern\x3270-$i
 }
