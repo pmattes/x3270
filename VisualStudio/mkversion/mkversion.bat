043a3c6e12f6d1d23d3680789e8b848c61d94fb7
Paul Mattes <paul.mattes@case.edu>
2022-01-02 15:19:00 -0600

    Remove w prefixes from version in VS buids

--- VisualStudio/mkversion/mkversion.bat
+++ VisualStudio/mkversion/mkversion.bat
@@ -1,11 +1,11 @@
 cd ..\..\wc3270
-%1\mkversion.exe wc3270 ..\Common\version.txt
-%1\mkversion.exe -w wc3270 ..\Common\version.txt
+%1\mkversion.exe c3270 ..\Common\version.txt
+%1\mkversion.exe -w c3270 ..\Common\version.txt
 cd ..\ws3270
-%1\mkversion.exe ws3270 ..\Common\version.txt
+%1\mkversion.exe s3270 ..\Common\version.txt
 cd ..\wpr3287
-%1\mkversion.exe wpr3287 ..\Common\version.txt
+%1\mkversion.exe pr3287 ..\Common\version.txt
 cd ..\wb3270
-%1\mkversion.exe wb3270 ..\Common\version.txt
+%1\mkversion.exe b3270 ..\Common\version.txt
 cd ..\x3270if
 %1\mkversion.exe x3270if ..\Common\version.txt
