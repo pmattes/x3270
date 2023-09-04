2b2cafb7f0e47d89a2df2b6371a34ac0941a20f0
Paul Mattes <paul.mattes@case.edu>
2023-09-04 16:21:09 -0500

    Fix multi-line error message in s3270

---
+++ s3270/Test/testConnectFail.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2021-2023 Paul Mattes.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the names of Paul Mattes nor the names of his contributors
+#       may be used to endorse or promote products derived from this software
+#       without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY PAUL MATTES "AS IS" AND ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+# EVENT SHALL PAUL MATTES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# s3270 connection fail tests
+
+import unittest
+from subprocess import Popen, PIPE, DEVNULL
+import requests
+import os
+import Common.Test.playback as playback
+import Common.Test.cti as cti
+
+class TestS3270ConnectFail(cti.cti):
+
+    # s3270 connect fail test
+    def test_s3270_connect_fail(self):
+
+        # Start s3270.
+        s3270 = Popen(cti.vgwrap(["s3270"]), stdin=PIPE, stdout=PIPE)
+        self.children.append(s3270)
+
+        # Push a trivial command at it.
+        s3270.stdin.write(b'Connect(localhost:1)\n')
+
+        # Decode the result.
+        stdout = s3270.communicate()[0].decode('utf8').split(os.linesep)
+
+        # Wait for the process to exit successfully.
+        self.vgwait(s3270)
+
+        # Test the output.
+        self.assertEqual(4, len(stdout))
+        self.assertEqual('data: Connection failed: localhost, port 1: Connection refused', stdout[0])
+        self.assertTrue(stdout[1].startswith('L U U N N 4 24 80 0 0 0x0 '))
+        self.assertEqual('error', stdout[2])
+        self.assertEqual('', stdout[3])
+
+if __name__ == '__main__':
+    unittest.main()
