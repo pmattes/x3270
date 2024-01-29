c4d7beb4ce41a88dd4f9ab86192770639361633d
Paul Mattes <paul.mattes@case.edu>
2024-01-28 18:32:41 -0600

    Fix String() \u issue

--- s3270/Test/testString.py
+++ s3270/Test/testString.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Copyright (c) 2021-2022 Paul Mattes.
+# Copyright (c) 2021-2024 Paul Mattes.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -66,6 +66,38 @@ class TestS3270String(cti.cti):
             _, row, _, column, *_ = result.split()
             self.assertEqual(8, int(row), 'Cursor is on the wrong row')
             self.assertEqual(36, int(column), 'Cursor is on the wrong coluumn')
+
+        # Wait for the processes to exit.
+        requests.get(f'http://127.0.0.1:{sport}/3270/rest/json/Quit()')
+        self.vgwait(s3270)
+
+    # s3270 String() \u bugfix test.
+    def test_s3270_string_slash_u(self):
+
+        pport, socket = cti.unused_port()
+        with playback.playback(self, 's3270/Test/ibmlink.trc', pport) as p:
+            self.check_listen(pport)
+            socket.close()
+
+            # Start s3270.
+            sport, socket = cti.unused_port()
+            s3270 = Popen(cti.vgwrap(['s3270', '-httpd', str(sport), f'127.0.0.1:{pport}', '-codepage', '424']),
+                            stdin=DEVNULL, stdout=DEVNULL)
+            self.children.append(s3270)
+            self.check_listen(sport)
+            socket.close()
+
+            # Fill in the screen.
+            p.send_records(4)
+
+            # Enter a Unicode character above U+00FF.
+            requests.get(f'http://127.0.0.1:{sport}/3270/rest/json/String("\\u0061\\u05E9\\u0062")')
+
+            # Make sure the cursor lands in the right spot.
+            r = requests.get(f'http://127.0.0.1:{sport}/3270/rest/json/ascii1(21,13,1,3)')
+            self.assertEqual(requests.codes.ok, r.status_code)
+            result = r.json()['result'][0]
+            self.assertEqual("aשb", result, 'Expected text is wrong')
 
         # Wait for the processes to exit.
         requests.get(f'http://127.0.0.1:{sport}/3270/rest/json/Quit()')
