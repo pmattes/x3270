52b12ec61a980487f23f589a54454ec777cf86fb
Paul Mattes <paul.mattes@case.edu>
2025-05-25 13:38:21 -0500

    Makefile rule overhaul

--- confdefs.h
+++
@@ -1,7 +0,0 @@
-/* confdefs.h */
-#define PACKAGE_NAME "suite3270"
-#define PACKAGE_TARNAME "suite3270"
-#define PACKAGE_VERSION "4.4"
-#define PACKAGE_STRING "suite3270 4.4"
-#define PACKAGE_BUGREPORT ""
-#define PACKAGE_URL ""
