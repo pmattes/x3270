fbbfca856a2407a396f2d7f1e84a6d28ebef0a5f
Volodymyr Borysenko <weissekat@gmail.com>
2025-09-28 04:53:54 +0300

    Add CP1158 Ukrainian with Euro sign codepage

--- wc3270/relink.c
+++ wc3270/relink.c
@@ -93,6 +93,7 @@ codepages_t codepages[] = {
     { "uk",		"285",	0, L"1252"	},
     { "uk-euro",	"1146",	0, L"1252"	},
     { "ukrainian",	"1123",	0, L"1251"	},
+    { "ukrainian-euro",	"1158",	0, L"1251"	},
     { "us-euro",	"1140",	0, L"1252"	},
     { "us-intl",	"037",	0, L"1252"	},
     { NULL,		NULL,	0, NULL	}
